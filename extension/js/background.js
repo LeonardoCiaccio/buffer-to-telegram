9bf5e2c01cc46681bb16884d3457195574129d39
Leonardo <leonardo.ciaccio@gmail.com>
2018-11-12 12:45:41 +0100

    Fixed minor bugs
    When retweet now share link of tweet

--- extension/js/background.js
+++ extension/js/background.js
@@ -98,16 +98,37 @@
   chrome.webRequest.onBeforeRequest.addListener( function( details ){
 
     var
-      photo = ( details.requestBody.formData[ "media[picture]" ] && details.requestBody.formData[ "media[picture]" ].length > 0 ) ? details.requestBody.formData[ "media[picture]" ][ 0 ] : ""
+      retweet = ( details.requestBody.formData[ "retweet[url]" ] && details.requestBody.formData[ "retweet[url]" ].length > 0 ) ? details.requestBody.formData[ "retweet[url]" ][ 0 ] : ""
+
+      ,photo = ( details.requestBody.formData[ "media[picture]" ] && details.requestBody.formData[ "media[picture]" ].length > 0 ) ? details.requestBody.formData[ "media[picture]" ][ 0 ] : ""
+
+      ,photoTitle = ( details.requestBody.formData[ "media[title]" ] && details.requestBody.formData[ "media[title]" ].length > 0 ) ? details.requestBody.formData[ "media[title]" ][ 0 ] : ""
+
+      ,photoDescription = ( details.requestBody.formData[ "media[description]" ] && details.requestBody.formData[ "media[description]" ].length > 0 ) ? details.requestBody.formData[ "media[description]" ][ 0 ] : ""
 
       ,text = ( details.requestBody.formData.text && details.requestBody.formData.text.length > 0  ) ? details.requestBody.formData.text[ 0 ] : ""
-
-      ,currentSession = photo + text
     ;
 
-// --> Only first item
+// --> Only first item with one or more params
 
-    if( sended === true )return false;
+    photo = ( photo && photo.length > 0 ) ? photo.trim() : "";
+    text  = ( text && text.length > 0 ) ? text.trim() : "";
+
+    if( retweet && retweet.length > 0 ){
+
+      text = retweet.trim();
+
+    }else if( text.length == 0 && photoDescription && photoDescription.length > 0 ){
+
+      text = photoDescription.trim();
+
+    }else if( text.length == 0 && photoTitle && photoTitle.length > 0 ){
+
+      text = photoTitle.trim();
+
+    }
+
+    if( sended === true || ( photo.length == 0 && text.length == 0 ) )return false;
 
     sended = true;
 
