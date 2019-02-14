998c6889eb769c19e3c760b364d88b9121ee4844
Leonardo <leonardo.ciaccio@gmail.com>
2019-02-14 09:49:47 +0100

    Updated Buffer API

--- extension/js/background.js
+++ extension/js/background.js
@@ -97,36 +97,29 @@
 
   chrome.webRequest.onBeforeRequest.addListener( function( details ){
 
-    var
-      retweet = ( details.requestBody.formData[ "retweet[url]" ] && details.requestBody.formData[ "retweet[url]" ].length > 0 ) ? details.requestBody.formData[ "retweet[url]" ][ 0 ] : ""
-
-      ,photo = ( details.requestBody.formData[ "media[picture]" ] && details.requestBody.formData[ "media[picture]" ].length > 0 ) ? details.requestBody.formData[ "media[picture]" ][ 0 ] : ""
-
-      ,photoTitle = ( details.requestBody.formData[ "media[title]" ] && details.requestBody.formData[ "media[title]" ].length > 0 ) ? details.requestBody.formData[ "media[title]" ][ 0 ] : ""
-
-      ,photoDescription = ( details.requestBody.formData[ "media[description]" ] && details.requestBody.formData[ "media[description]" ].length > 0 ) ? details.requestBody.formData[ "media[description]" ][ 0 ] : ""
-
-      ,text = ( details.requestBody.formData.text && details.requestBody.formData.text.length > 0  ) ? details.requestBody.formData.text[ 0 ] : ""
+    var 
+      args, photo, text      
     ;
+
+    try {
+      
+      var params = decodeURIComponent( String.fromCharCode.apply( null, new Uint8Array( details.requestBody.raw[ 0 ].bytes ) ) );
+      
+      args = JSON.parse( JSON.parse( params ).args ).args;
+      
+      text = args.text || "";
+      photo = (args.media && args.media.photo) ? args.media.photo : "";
+
+    } catch (e) {
+      
+      return false;
+
+    }
 
 // --> Only first item with one or more params
 
     photo = ( photo && photo.length > 0 ) ? photo.trim() : "";
     text  = ( text && text.length > 0 ) ? text.trim() : "";
-
-    if( retweet && retweet.length > 0 ){
-
-      text = retweet.trim();
-
-    }else if( text.length == 0 && photoDescription && photoDescription.length > 0 ){
-
-      text = photoDescription.trim();
-
-    }else if( text.length == 0 && photoTitle && photoTitle.length > 0 ){
-
-      text = photoTitle.trim();
-
-    }
 
     if( sended === true || ( photo.length == 0 && text.length == 0 ) )return false;
 
@@ -165,7 +158,7 @@
   },{
       "urls"    : [
 
-          "https://buffer.com/api/1/updates/create.json"
+          "https://publish.buffer.com/rpc"
       ]
 
       ,"types"  : [
