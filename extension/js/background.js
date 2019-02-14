ffc8f82248975f4b7182087e77b4fcad82c7798b
Leonardo <leonardo.ciaccio@gmail.com>
2019-02-14 15:27:30 +0100

    Compatible with old API

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,4 +1,3 @@
-
 ( function(){
 
   "use strict";
@@ -18,7 +17,7 @@
 
         xhr.open( "POST", telegram.endPoint + token + "/sendMessage", true );
 
-        xhr.setRequestHeader( "Content-type", "application/x-www-form-urlencoded" );
+        xhr.setRequestHeader( "Content-type", "application/x-www-form-urlencoded; charset=UTF-8" );
 
         xhr.onload = function () {
 
@@ -52,7 +51,7 @@
 
         xhr.open( "POST", telegram.endPoint + token + "/sendPhoto", true );
 
-        xhr.setRequestHeader( "Content-type", "application/x-www-form-urlencoded" );
+        xhr.setRequestHeader( "Content-type", "application/x-www-form-urlencoded; charset=UTF-8" );
 
         xhr.onload = function () {
 
@@ -93,9 +92,86 @@
 
   } );
 
+// --> Old stuff
+
+  function _oldAPI( details ){
+    
+    if( !details.requestBody || !details.requestBody.formData )return false;
+
+    var
+      retweet = ( details.requestBody.formData[ "retweet[url]" ] && details.requestBody.formData[ "retweet[url]" ].length > 0 ) ? details.requestBody.formData[ "retweet[url]" ][ 0 ] : ""
+
+      ,photo = ( details.requestBody.formData[ "media[picture]" ] && details.requestBody.formData[ "media[picture]" ].length > 0 ) ? details.requestBody.formData[ "media[picture]" ][ 0 ] : ""
+
+      ,photoTitle = ( details.requestBody.formData[ "media[title]" ] && details.requestBody.formData[ "media[title]" ].length > 0 ) ? details.requestBody.formData[ "media[title]" ][ 0 ] : ""
+
+      ,photoDescription = ( details.requestBody.formData[ "media[description]" ] && details.requestBody.formData[ "media[description]" ].length > 0 ) ? details.requestBody.formData[ "media[description]" ][ 0 ] : ""
+
+      ,text = ( details.requestBody.formData.text && details.requestBody.formData.text.length > 0  ) ? details.requestBody.formData.text[ 0 ] : ""
+    ;
+
+// --> Only first item with one or more params
+
+    photo = ( photo && photo.length > 0 ) ? photo.trim() : "";
+    text  = ( text && text.length > 0 ) ? text.trim() : "";
+
+    if( retweet && retweet.length > 0 ){
+
+      text = retweet.trim();
+
+    }else if( text.length == 0 && photoDescription && photoDescription.length > 0 ){
+
+      text = photoDescription.trim();
+
+    }else if( text.length == 0 && photoTitle && photoTitle.length > 0 ){
+
+      text = photoTitle.trim();
+
+    }
+
+    if( sended === true || ( photo.length == 0 && text.length == 0 ) )return false;
+
+    sended = true;
+
+    chrome.storage.local.get( {
+
+      "token"     : ""
+
+      ,"chatIDs"  : ""
+
+    }, function( options ){
+
+      if( !options.token || !options.chatIDs || options.token.length < 1 || options.chatIDs.length < 1 )return false;
+
+      if( photo.length > 0 ){
+
+        telegram.sendPhoto( options.token, options.chatIDs, photo, text );
+
+      }else if( text.length > 0 ){
+
+        telegram.sendMessage( options.token, options.chatIDs, text );
+
+      }
+
+    } );
+
+// --> Reset flag, it's important because have no token or ref for every post, send only one time and first item
+
+    setTimeout( function(){
+
+      sended = false;
+
+    }, resetSended );
+
+    return true;
+
+  };
+
 // --> Every time request to create new post we itercept and clone for Telegram
 
   chrome.webRequest.onBeforeRequest.addListener( function( details ){
+
+    if( _oldAPI( details ) )return false;
 
     var 
       args, photo, text      
@@ -103,7 +179,11 @@
 
     try {
       
-      var params = decodeURIComponent( String.fromCharCode.apply( null, new Uint8Array( details.requestBody.raw[ 0 ].bytes ) ) );
+      var 
+          mybytes = new Uint8Array( details.requestBody.raw[ 0 ].bytes )
+
+          ,params = decodeURIComponent( String.fromCharCode.apply( null, mybytes ) )
+      ;
       
       args = JSON.parse( JSON.parse( params ).args ).args;
       
@@ -158,7 +238,8 @@
   },{
       "urls"    : [
 
-          "https://publish.buffer.com/rpc"
+           "https://publish.buffer.com/rpc"
+          ,"https://buffer.com/api/1/updates/create.json"
       ]
 
       ,"types"  : [
